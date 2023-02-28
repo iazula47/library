0d73e8a26347a9bf1d9f9f761ca4082e2748ab70
unknown <cit183e-31@M303.UC-BCF.EDU.PH>
2023-02-28 17:48:49 +0800

    additionals

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,17 @@
+<div class="col-lg-4 col-md-6">
+            <div class="footer-info">
+              <h3>Books Information</h3>
+              <p>
+                A108 Adam Street <br>
+                NY 535022, USA<br><br>
+                <strong>Phone:</strong> +1 5589 55488 55<br>
+                <strong>Email:</strong> info@example.com<br>
+              </p>
+              <div class="social-links d-flex mt-3">
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-twitter"></i></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-facebook"></i></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-instagram"></i></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
